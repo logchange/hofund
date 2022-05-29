0df9ab286253ff24ddd0272cc3e7a923685064b5
Peter <peter.zmilczak@gmail.com>
2022-05-29 22:31:39 +0200

    test app template

---
+++ example/test-app/src/main/java/dev/hofund/testapp/testapp/TestAppApplication.java
@@ -0,0 +1,13 @@
+package dev.hofund.testapp.testapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAppApplication.class, args);
+	}
+
+}
---
+++ example/test-app/src/test/java/dev/hofund/testapp/testapp/TestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hofund.testapp.testapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
