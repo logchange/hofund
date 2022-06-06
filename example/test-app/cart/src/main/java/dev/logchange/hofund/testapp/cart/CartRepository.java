8fbf6896ee40ff9d68f250e9817ec38c8b88d375
Peter <peter.zmilczak@gmail.com>
2022-06-06 19:51:29 +0200

    moved example to separate repo, fixed pom.xml

--- example/test-app/cart/src/main/java/dev/logchange/hofund/testapp/cart/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.logchange.hofund.testapp.cart;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartRepository extends JpaRepository<Cart, Long> {
-}
--- example/test-app/products/src/main/java/dev/logchange/hofund/testapp/products/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.logchange.hofund.testapp.products;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
